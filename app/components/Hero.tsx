d3e6c46c42bfa25e739e3d839360a511fae6cadb
nordstromjimmy <nordstromjimmy@gmail.com>
2026-02-22 18:28:45 +0100

    update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -13,6 +13,7 @@ export default function Hero() {
     "/screenshots/screenshot1.png",
     "/screenshots/screenshot2.png",
     "/screenshots/screenshot3.png",
+    "/screenshots/screenshot4.png",
   ];
 
   useEffect(() => {
