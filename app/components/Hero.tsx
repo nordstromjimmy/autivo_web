ec8fa10c4db5a65a80316d32838e595eb415d5ff
nordstromjimmy <nordstromjimmy@gmail.com>
2026-02-27 16:43:28 +0100

    update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -66,11 +66,11 @@ export default function Hero() {
             <div className="flex items-center gap-1 mb-6">
               {/* Logo Image (A with car) */}
               <Image
-                src="/weblogo2.png"
+                src="/weblogo12.png"
                 alt="Autivo"
                 width={80}
                 height={80}
-                className="w-16 h-16 sm:w-20 sm:h-20 md:w-36 md:h-36"
+                className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24"
                 priority
               />
 
